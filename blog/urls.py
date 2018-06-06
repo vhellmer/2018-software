db47e6fd57db048de47aa3e960f7aeef9e6f7f5e
Valerie Hellmer <valerie.hellmer@gmail.com>
2018-06-06 17:47:26 -0500

    Fixed links on nav bar

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.front_page, name='front_page'),
+    url(r'^software/$', views.software, name='software'),
+    url(r'^discussion/$', views.discussion, name='discussion'),
+    url(r'^about/$', views.about, name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-def post_list(request):
-    return render(request, 'front_page/front_page.html', {})
+def front_page(request):
+    return render(request, 'website/front_page.html', {})
+
+def software(request):
+	return render(request, 'website/software.html', {})
+
+def discussion(request):
+	return render(request, 'website/discussion.html', {})
+
+def about(request):
+	return render(request, 'website/about.html', {})
