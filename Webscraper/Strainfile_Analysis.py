c577658ea25ccc4bd79d71fe8734763c86f4e6e3
Valerie Hellmer <valerie.hellmer@gmail.com>
2018-10-02 18:40:32 -0500

    Did some basic analysis on the reformatted text file, still need to make graphs

--- Webscraper/Strainfile_Analysis.py
+++ Webscraper/Strainfile_Analysis.py
@@ -17,15 +17,44 @@
 # print(counts)
 # print(names)
 
-strain_file = open("strainfile.txt","r").read()
+from collections import defaultdict
+
+strain_file = open("strainfile-Reformatted.txt","r").read()
 
 words_file = open("words.txt", "r").read()
-words = words_file.split("\n")
+common_words = words_file.split("\n")
 
 possible_strains = []
 lines = strain_file.split("*****")
-print(lines)
 for line in lines:
-        if line not in words:
-            possible_strains += line
+    line = line.replace("\n", " ")
+    line = line.replace(",", " ")
+    line = line.replace(".", " ")
+    line = line.replace("(", " ")
+    line = line.replace(":", " ")
+    line = line.replace(")", " ")
+
+    words = line.split(" ")
+    team_words = []
+    for word in words:
+        if word.lower() not in common_words:
+            team_words.append(word.lower())
+    possible_strains.append(team_words)
+
 print(possible_strains)
+
+common_names = ["coli", "subtilis", "crescentus", "fischeri", "cerevisiae", "pastoris", "bassiana", "tumefa"]
+common_dict = defaultdict(int)
+all_dict = defaultdict(int)
+
+for team_list in possible_strains:
+    for word in set(team_list):
+        for name in common_names:
+            if name in word:
+                common_dict[name] += 1
+        all_dict[word] += 1
+
+print(common_dict)
+for key, value in sorted(all_dict.items()):
+    print(key, value)
+print("NUMBER OF TEAMS: " + str(len(possible_strains)))
